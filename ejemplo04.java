be0e53c28334a8617dc2a684fea121fc8b85f8b6
Marco <44386708+marcocesar123@users.noreply.github.com>
2018-10-24 18:59:40 -0500

    Create ejemplo04.java

---
+++ ejemplo04.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ejemplo04{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int num;
+System.out.println("Introduzca numero: ");
+num = sc.nextInt();
+if ((num%2)==0)
+System.out.println("PAR");
+else
+System.out.println("IMPAR");
+}
+}
