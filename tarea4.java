06abd77d64cfbc0531bf9d4c8e5bf5a670cff653
Marco <44386708+marcocesar123@users.noreply.github.com>
2018-10-24 18:55:40 -0500

    Create tarea4.java

---
+++ tarea4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Tarea4{
+public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+System.out.println("INGRESE UN NUMER0");
+ double num= sc.nextDouble();
+	if (num== 0)
+	System.out.println ("EL NÚMERO ES NEUTRO");
+	else if (num>0)
+	System.out.println ("EL NUMERO ES POSITIVO");
+	else
+	System.out.println ("EL NUMERO ES NEGATIVO");
+	
+  }
+ }
