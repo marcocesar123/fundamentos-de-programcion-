ac42a0cbb71cf5cd4e335b56a29d6193fc9ad3d8
Marco <44386708+marcocesar123@users.noreply.github.com>
2018-10-25 17:24:05 -0500

    Create Ejemplo13.java

---
+++ Ejemplo13.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Ejemplo13 {
+public static void main(String[] args) {
+int num;
+Scanner sc = new Scanner(System.in);
+System.out.print("Introduzca un número: ");
+num = sc.nextInt();
+while (num !.= 0){
+num = num positivo;
+System.out.print("Introduzca un número: ");
+num = sc.nextInt();
+}
+while (num!.= -0){
+num = num negativo;
+System.out.print("Introduzca un número: ");
+num = sc.nextInt();
+}
+}
